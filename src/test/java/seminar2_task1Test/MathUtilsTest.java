2fc39fd1c7045dbdc81a5b989171982ea8633e1e
Biramax <biramax@biramax>
2023-10-12 14:12:25 +0300

    Hometask on Seminar #2 done!

---
+++ src/test/java/seminar2_task1Test/MathUtilsTest.java
@@ -0,0 +1,20 @@
+package seminar2_task1Test;
+
+import seminar2_task1.MathUtils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource({"2, 3, 6",
+                "0, 5, 0",
+                "-2, -4, 8",
+                "10, -2, -20"})
+    public void testMultiply(int a, int b, int expectedResult) {
+        int result = MathUtils.multiply(a, b);
+        assertEquals(expectedResult, result, "Некорректное умножение");
+    }
+}
