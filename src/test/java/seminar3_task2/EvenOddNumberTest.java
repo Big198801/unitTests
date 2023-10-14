f2279857cd89b6ca4b02fe5ed8067c5541448090
Biramax <biramax@biramax>
2023-10-14 22:38:37 +0300

    Hometask on Seminar #3 done!

--- src/test/java/seminar3_task2/EvenOddNumberTest.java
+++ src/test/java/seminar3_task2/EvenOddNumberTest.java
@@ -11,8 +11,13 @@ public class EvenOddNumberTest {
     @ParameterizedTest
     @CsvSource({"2, true", "3, false"})
     public void isEvenNumberTest(int n, boolean expectedResult) {
+
+        // Создаём эксземпляр класса
         EvenOddNumber evenOddNumber = new EvenOddNumber();
+
+        // Проверяем работу метода isEvenNumber на параметрах из @CsvSource
         boolean result = evenOddNumber.isEvenNumber(n);
+
         assertEquals(expectedResult, result); // JUnit
         assertThat(result).isEqualTo(expectedResult); // AssertJ
     }
--- src/test/java/seminar3_task3/NumberInIntervalTest.java
+++ src/test/java/seminar3_task3/NumberInIntervalTest.java
@@ -11,8 +11,13 @@ public class NumberInIntervalTest {
     @ParameterizedTest
     @CsvSource({"2, false", "33, true", "25, true", "100, true", "188, false"})
     public void numberInIntervalTest(int n, boolean expectedResult) {
+
+        // Создаём эксземпляр класса
         NumberInInterval task = new NumberInInterval();
+
+        // Проверяем работу метода numberInInterval на параметрах из @CsvSource
         boolean result = task.numberInInterval(n);
+
         assertEquals(expectedResult, result); // JUnit
         assertThat(result).isEqualTo(expectedResult); // AssertJ
     }
